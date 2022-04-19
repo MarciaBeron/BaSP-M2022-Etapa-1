80bdc13ef758766068adb5c5f14ae9239a48a4de
unknown <mtatianabp@gmail.com>
2022-04-19 10:44:53 -0300

    BaSP-W5-2022: Created folder semana-05, index and first javascript archive for exercise 01

---
+++ semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,19 @@
+/*Crear dos variables numéricas y utilizar el operador suma para guardar
+ el valor de la suma de ambos números en una 3er variable.*/
+var num1 = 35;
+var num2 = 5;
+var addition = num1 + num2;
+console.log('solution01_a:', addition);
+/*Crear dos variables de tipo String y 
+concatenarlas guardando el resultado en una 3er variable*/
+var stringA = "Hello";
+var stringB = " world";
+var Greetings = stringA + stringB;
+console.log('solution01_b:', Greetings);
+/* Crear dos variables de tipo String y sumar el largo de cada variable 
+(cantidad de letras del string) guardando el resultado de la suma en una 3er 
+variable (utilizar length). */ 
+var stringC = "Goodbye";
+var stringD = "everybody";
+var additionString = stringC + stringD;
+console.log('solution01_c:', additionString.length);
